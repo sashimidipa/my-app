8d8e96188d8bd6d6cf0e93a2787b240919af66fc
Sasha Alexandra Di Paolo <113391000+sashimidipa@users.noreply.github.com>
2023-03-22 21:09:41 -0300

    biggining my first app in React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const BookList = () => {
+    return <section className='booklist'>
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    </section>
+}
+const Book = () => {
+    return(
+        <article className='book'>
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    );
+}
+
+const Image = () => <img src="./images/book-1.jpg" alt="Atomic Habits" />;
+const Title = () => <h2>Atomic Habits</h2>;
+const Author = () => {
+    return <h4>James Clear</h4>}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
